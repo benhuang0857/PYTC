dbd68419a13312f16e6c0a29bac6ada0c115cc40
benhuang0857 <benhuang0857@gmail.com>
2021-10-27 00:55:28 +0800

    first proc test

--- app/Http/Controllers/ProcedureController.php
+++ app/Http/Controllers/ProcedureController.php
@@ -7,10 +7,56 @@ use DB;
 
 class ProcedureController extends Controller
 {
+    #Mysql Call Proc Login_Proc
     public function LoginProc(Request $req)
     {
         $json = json_encode($req);
         DB::select('call Login_Proc(?, @out)', [$json]);
+        $selectResult = DB::select('SELECT @out AS result');
+        dd($selectResult);
+    }
+
+    #Mysql Call Proc List_Insert_Proc
+    public function ListInsertProc(Request $req)
+    {
+        $json = json_encode($req);
+        DB::select('call List_Insert_Proc(?, @out)', [$json]);
+        $selectResult = DB::select('SELECT @out AS result');
+        dd($selectResult);
+    }
+
+    #Mysql Call Proc List_Select_Proc
+    public function ListSelectProc(Request $req)
+    {
+        $json = json_encode($req);
+        DB::select('call List_Select_Proc(?, @out)', [$json]);
+        $selectResult = DB::select('SELECT @out AS result');
+        dd($selectResult);
+    }
+
+    #Mysql Call Proc User_Insert_Proc
+    public function UserInsertProc(Request $req)
+    {
+        $json = json_encode($req);
+        DB::select('call User_Insert_Proc(?, @out)', [$json]);
+        $selectResult = DB::select('SELECT @out AS result');
+        dd($selectResult);
+    }
+
+    #Mysql Call Proc User_Insert_Proc
+    public function UserSelectProc(Request $req)
+    {
+        $json = json_encode($req);
+        DB::select('call User_Select_Proc(?, @out)', [$json]);
+        $selectResult = DB::select('SELECT @out AS result');
+        dd($selectResult);
+    }
+
+    #Mysql Call Proc User_Update_Proc
+    public function UserUpdateProc(Request $req)
+    {
+        $json = json_encode($req);
+        DB::select('call User_Update_Proc(?, @out)', [$json]);
         $selectResult = DB::select('SELECT @out AS result');
         dd($selectResult);
     }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//請在URL加上api/{route_name}這樣才有辦法calling下方API
 Route::get('login', 'ProcedureController@LoginProc');
+Route::get('list/insert', 'ProcedureController@ListInsertProc');
+Route::get('list/select', 'ProcedureController@ListSelectProc');
+Route::get('user/insert', 'ProcedureController@UserInsertProc');
+Route::get('user/select', 'ProcedureController@UserSelectProc');
+Route::get('user/update', 'ProcedureController@UserUpdateProc');
