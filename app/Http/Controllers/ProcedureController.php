bdb38a91fe2f5d2786494d378ba8fd2524596d62
benhuang0857 <benhuang0857@gmail.com>
2021-10-27 00:13:59 +0800

    first proc test

--- app/Http/Controllers/ProcedureController.php
+++ app/Http/Controllers/ProcedureController.php
@@ -10,7 +10,8 @@ class ProcedureController extends Controller
     public function LoginProc(Request $req)
     {
         $out = "";
-        $reqJson = json_encode($req);
+        //$reqJson = json_encode($req);
+        $reqJson = "json_object('email','test4','password','test')";
         $out = "";
         DB::select('call Login_Proc(?,?)', [$reqJson, $out]);
 
