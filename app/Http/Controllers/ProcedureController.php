b2f84fa70d419e7bb3fe7329c82e0110e1cecc72
benhuang0857 <benhuang0857@gmail.com>
2021-10-26 22:47:36 +0800

    first proc test

---
+++ app/Http/Controllers/ProcedureController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class ProcedureController extends Controller
+{
+    public function LoginProc(Request $req)
+    {
+        $email = $req->user_email;
+        $passwd = $req->user_password;
+        $login = DB::select(
+            'call Login_Proc(?,?)', [$email, $passwd]
+        );
+    }
+}
