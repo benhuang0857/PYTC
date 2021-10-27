7be5cef604cf1ce669f713d384863484f32aadd3
benhuang0857 <benhuang0857@gmail.com>
2021-10-27 22:51:28 +0800

    Add Cors Middleware

--- app/Http/Controllers/ProcedureController.php
+++ app/Http/Controllers/ProcedureController.php
@@ -10,9 +10,9 @@ class ProcedureController extends Controller
     #Mysql Call Proc Login_Proc
     public function LoginProc(Request $req)
     {
-        //$json = json_encode($req);
-        $json = 'json_object("email","test4","password","test")';
-        DB::select('call Login_Proc(?, @out)', [$json]);
+        $json = json_encode($req);
+        //$json = 'json_object("email","test4","password","test")';
+        DB::select('call Login_Proc(:a, @out)', [$a => $json]);
         $selectResult = DB::select('SELECT @out AS result');
         return $selectResult;
     }
