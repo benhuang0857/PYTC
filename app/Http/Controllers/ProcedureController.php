b67017697945d2c6f37faddd3249bbefffe10bee
benhuang0857 <benhuang0857@gmail.com>
2021-10-29 22:41:23 +0800

    Add Cors Middleware

--- app/Http/Controllers/ProcedureController.php
+++ app/Http/Controllers/ProcedureController.php
@@ -20,7 +20,7 @@ class ProcedureController extends Controller
         // str_replace('[','', $selectResult);
         // str_replace(']','', $selectResult);
 
-        dd($selectResult);
+        dd($selectResult[0]);
     }
 
     #Mysql Call Proc List_Insert_Proc
