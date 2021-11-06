d6f77778547f28293144266c1f8b975bde28a9cb
benhuang0857 <benhuang0857@gmail.com>
2021-11-06 14:53:23 +0800

    Add Cors Middleware

--- app/Http/Controllers/ProcedureController.php
+++ app/Http/Controllers/ProcedureController.php
@@ -14,22 +14,24 @@ class ProcedureController extends Controller
         $id         = $req->email;
         $password   = $req->password;
 
-        try {
-            $user = User::where('id', $id)->where('password', $password)->firstOrFail();
-            //dd($user);
-            return json_encode(
-                array(
-                    'email' => $id,
-                    'status' => $user->isEnable
-                ), JSON_UNESCAPED_UNICODE);
-        } catch (\Throwable $th) {
+        $q = DB::raw('SELECT Login_Func('.$id.', '.$password.')');
+        dd($q);
+        // try {
+        //     $user = User::where('id', $id)->where('password', $password)->firstOrFail();
+        //     //dd($user);
+        //     return json_encode(
+        //         array(
+        //             'email' => $id,
+        //             'status' => $user->isEnable
+        //         ), JSON_UNESCAPED_UNICODE);
+        // } catch (\Throwable $th) {
             
-            return response(json_encode(
-                array(
-                    //'errorMsg' => 'User Not Found'
-                )
-            ), 404)->header('Content-Type', 'application/json');
-        }
+        //     return response(json_encode(
+        //         array(
+        //             //'errorMsg' => 'User Not Found'
+        //         )
+        //     ), 404)->header('Content-Type', 'application/json');
+        // }
     }
 
     #Mysql Call Proc List_Insert_Proc 開發中
