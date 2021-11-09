79b47ebeab4f97b70a487086589966cadc4f1cd4
benhuang0857 <benhuang0857@gmail.com>
2021-11-10 00:06:25 +0800

    20211110

--- app/Http/Controllers/ProcedureController.php
+++ app/Http/Controllers/ProcedureController.php
@@ -14,7 +14,8 @@ class ProcedureController extends Controller
         $id         = $req->email;
         $password   = $req->password;
 
-        try {
+        try
+        {
             $user = User::where('id', $id)->where('password', $password)->firstOrFail();
             //dd($user);
             return json_encode(
@@ -32,75 +33,35 @@ class ProcedureController extends Controller
         }
     }
 
-    #Mysql Call Proc List_Insert_Proc 開發中
-    public function ListInsertProc(Request $req)
+    #Mysql Call Proc User_Insert_Proc
+    public function UserInsertProc(Request $req)
     {
-        $gid = $req->gid;
-        $gname = $req->gname;
-        $mid = $req->mid;
-        $id = $req->id;
-        $item_no = $req->item_no;
-        $item_name = $req->item_name;
-        $upd_user = $req->upd_user;
-        $json = 'json_object("gid","'.$gid.'","gname","'.$gname.'","mid","'.$mid.'","id","'.$id.'","item_no","'.$item_no.'","item_name","'.$item_name.'","upd_user","'.$upd_user.'")';
+        $id         = $req->email;
+        $password   = $req->password;
+        $name       = $req->name;
+        $isEnable   = 'N';
+        $isCfm      = 'N';
+        $upd_date   = date("YMd");
+        $upd_time   = date("His");
 
-        DB::select('call List_Insert_Proc('.$json.', @out)');
-        $selectResult = DB::select('SELECT @out AS result');
-
-        dd($selectResult);
-        //return $selectResult;
-    }
-
-    #Mysql Call Proc List_Select_Proc
-    public function ListSelectProc(Request $req)
-    {
-        $func = $req->func;
-        $gid = $req->gid;
-        $mid = $req->mid;
-        $json = 'json_object("func","'.$func.'","gid","'.$gid.'","mid","'.$mid.'")';
-
-        DB::select('call List_Select_Proc('.$json.', @out)');
-        $selectResult = DB::select('SELECT @out AS result');
-
-        $jObj = json_decode($selectResult[0]->result);
-        if($jObj != null)
+        try
         {
-            return json_encode(
-                $jObj, JSON_UNESCAPED_UNICODE
-            );
-        }
-        else
-        {
+            $user = new User();
+            $user->id           = $id;
+            $user->password     = $password;
+            $user->name         = $password;
+            $user->isEnable     = $isEnable;
+            $user->isCfm        = $isCfm;
+            $user->upd_date     = $upd_date;
+            $user->upd_time     = $upd_time;
+
             return response(json_encode(
                 array(
                     //'errorMsg' => 'User Not Found'
                 )
-            ), 404)->header('Content-Type', 'application/json');
-        }
-    }
-
-    #Mysql Call Proc User_Insert_Proc 有異常
-    public function UserInsertProc(Request $req)
-    {
-        $name = $req->name;
-        $email = $req->email;
-        $password = $req->password;
-        $position = $req->position; //有異常
-        $upd_user = $req->upd_user;
-        //$json = 'json_object("name","'.$name.'","email","'.$email.'","password","'.$password.'","position","'.$position.'","upd_user","'.$upd_user.'")';
-
-        $json = 'JSON_OBJECT("name","Janee","email","Janeee@pyct.com","password","test","position",JSON_ARRAY(JSON_OBJECT("area",1,"unit",2),JSON_OBJECT("area",1,"unit",3)))';
-
-        DB::select('call User_Insert_Proc('.$json.', @out)');
-        $selectResult = DB::select('SELECT @out AS result');
-
-        $jObj = json_decode($selectResult[0]->result);
-        if($jObj != null)
-        {
-            return json_encode($jObj, JSON_UNESCAPED_UNICODE);
-        }
-        else
-        {
+            ), 200)->header('Content-Type', 'application/json');
+        } catch (\Throwable $th) {
+            
             return response(json_encode(
                 array(
                     //'errorMsg' => 'User Not Found'
@@ -153,6 +114,54 @@ class ProcedureController extends Controller
         $json = 'json_object("id","'.$id.'","name","'.$name.'","email","'.$email.'","password","'.$password.'","position","'.$position.'","enable_cd","'.$enable_cd.'","upd_user","'.$upd_user.'")';
 
         DB::select('call User_Update_Proc('.$json.', @out)');
+        $selectResult = DB::select('SELECT @out AS result');
+
+        $jObj = json_decode($selectResult[0]->result);
+        if($jObj != null)
+        {
+            return json_encode(
+                $jObj, JSON_UNESCAPED_UNICODE
+            );
+        }
+        else
+        {
+            return response(json_encode(
+                array(
+                    //'errorMsg' => 'User Not Found'
+                )
+            ), 404)->header('Content-Type', 'application/json');
+        }
+    }
+
+    
+    #Mysql Call Proc List_Insert_Proc 開發中
+    public function ListInsertProc(Request $req)
+    {
+        $gid = $req->gid;
+        $gname = $req->gname;
+        $mid = $req->mid;
+        $id = $req->id;
+        $item_no = $req->item_no;
+        $item_name = $req->item_name;
+        $upd_user = $req->upd_user;
+        $json = 'json_object("gid","'.$gid.'","gname","'.$gname.'","mid","'.$mid.'","id","'.$id.'","item_no","'.$item_no.'","item_name","'.$item_name.'","upd_user","'.$upd_user.'")';
+
+        DB::select('call List_Insert_Proc('.$json.', @out)');
+        $selectResult = DB::select('SELECT @out AS result');
+
+        dd($selectResult);
+        //return $selectResult;
+    }
+
+    #Mysql Call Proc List_Select_Proc
+    public function ListSelectProc(Request $req)
+    {
+        $func = $req->func;
+        $gid = $req->gid;
+        $mid = $req->mid;
+        $json = 'json_object("func","'.$func.'","gid","'.$gid.'","mid","'.$mid.'")';
+
+        DB::select('call List_Select_Proc('.$json.', @out)');
         $selectResult = DB::select('SELECT @out AS result');
 
         $jObj = json_decode($selectResult[0]->result);
