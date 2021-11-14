ebcf1ea3ec09d38585691c25c99dd822da8e3605
benhuang0857 <benhuang0857@gmail.com>
2021-11-14 23:05:39 +0800

    20211114

--- app/Http/Controllers/ProcedureController.php
+++ app/Http/Controllers/ProcedureController.php
@@ -36,7 +36,7 @@ class ProcedureController extends Controller
     public function UserInsertProc(Request $req)
     {
 
-        dd($req->units);
+        dd($req->email);
         try
         {
             $now = new DateTime();
